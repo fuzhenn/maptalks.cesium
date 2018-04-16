81c7e7da1c927802a390ee7e779520ea81ffb0bd
fuzhenn <fuzhen@maptalks.org>
2018-04-16 10:28:36 +0800

    update demo

--- dist/maptalks.cesium.es.js
+++ dist/maptalks.cesium.es.js
@@ -7,7 +7,7 @@
  * requires maptalks@<2.0.0 
  */
 import { CanvasLayer, Util, renderer } from 'maptalks';
-import { Cartesian3, Cartographic, Color, Ellipsoid, Globe, JulianDate, Scene, SkyAtmosphere } from 'Cesium';
+import { Cartesian3, Cartographic, Color, Ellipsoid, Globe, JulianDate, Scene } from 'Cesium';
 
 function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
 
@@ -65,6 +65,8 @@ var CesiumLayer = function (_maptalks$CanvasLayer) {
 
 CesiumLayer.mergeOptions(options);
 
+var backColor = [112, 112, 112, 255];
+
 var CeisumLayerRenderer = function (_maptalks$renderer$Ca) {
     _inherits(CeisumLayerRenderer, _maptalks$renderer$Ca);
 
@@ -111,19 +113,52 @@ var CeisumLayerRenderer = function (_maptalks$renderer$Ca) {
             scene3DOnly: true
         });
         this.scene = new Scene(sceneOptions);
+        this.scene.fog.enabled = false;
+        this.scene.backgroundColor = new Color(backColor[0] / 255, backColor[1] / 255, backColor[2] / 255, 1);
         this.scene.camera.constrainedAxis = Cartesian3.UNIT_Z;
-        this.scene.camera.fov = Math.PI / 2; //90
         this.globe = new Globe(Ellipsoid.WGS84);
-        this.globe.baseColor = Color.WHITE;
+        this.globe.baseColor = new Color(backColor[0] / 255, backColor[1] / 255, backColor[2] / 255, 1);
         this.scene.globe = this.globe;
-        this.scene.skyAtmosphere = new SkyAtmosphere();
-        //ol.proj.Units.METERS_PER_UNIT[ol.proj.Units.DEGREES] = 2 * Math.PI * 6370997 / 360;
+        // this.scene.globe.show = false;
+        // this.scene.skyAtmosphere = new Cesium.SkyAtmosphere();
     };
 
     CeisumLayerRenderer.prototype.clearCanvas = function clearCanvas() {
         if (!this.canvas) {
             return;
         }
+    };
+
+    CeisumLayerRenderer.prototype.getCanvasImage = function getCanvasImage() {
+        var canvasImage = _maptalks$renderer$Ca.prototype.getCanvasImage.call(this);
+        if (!canvasImage || !canvasImage.image) {
+            return canvasImage;
+        }
+        var canvas = canvasImage.image;
+        if (!this.buffer) {
+            this.buffer = document.createElement('canvas');
+        }
+        var buffer = this.buffer;
+        var w = buffer.width = canvas.width;
+        var h = buffer.height = canvas.height;
+        var ctx = buffer.getContext('2d');
+
+        ctx.drawImage(canvas, 0, 0);
+        var imgData = ctx.getImageData(0, 0, w, h);
+        var sourceData = imgData.data;
+        for (var i = 0, l = sourceData.length; i < l; i += 4) {
+            if (sourceData[i] === backColor[0] && sourceData[i + 1] === backColor[1] && sourceData[i + 2] === backColor[2] /* && sourceData[i + 3] === backColor[3] */) {
+                    sourceData[i + 3] = 0;
+                } else if (this.layer.options['gray']) {
+                var gray = [sourceData[i] + sourceData[i + 1] + sourceData[i + 2]] / 3;
+                sourceData[i] = gray;
+                sourceData[i + 1] = gray;
+                sourceData[i + 2] = gray;
+            }
+        }
+        ctx.putImageData(imgData, 0, 0);
+        canvasImage.image = buffer;
+        return canvasImage;
     };
 
     CeisumLayerRenderer.prototype.resizeCanvas = function resizeCanvas(canvasSize) {
@@ -161,16 +196,27 @@ var CeisumLayerRenderer = function (_maptalks$renderer$Ca) {
         if (!center) {
             return;
         }
+
+        var fov = void 0;
+        if (this.canvas.height > this.canvas.width) {
+            fov = map.getFov() * Math.PI / 180;
+        } else {
+            fov = map.getFov() * Math.PI / 180 * this.canvas.width / this.canvas.height;
+        }
+        scene.camera.frustum.fov = fov;
+
         var ll = center.toArray();
+        var pitch = toRadians(map.getPitch());
+
+        var distance = this._calcDistance(map);
 
         var carto = new Cartographic(toRadians(ll[0]), toRadians(ll[1]));
         if (scene.globe) {
             var height = scene.globe.getHeight(carto);
             carto.height = height || 0;
         }
-        // carto.height = 0;
         var destination = Ellipsoid.WGS84.cartographicToCartesian(carto);
-        var pitch = toRadians(map.getPitch());
+
         /** @type {Cesium.optionsOrientation} */
         var orientation = {
             pitch: pitch - Math.PI / 2,
@@ -181,56 +227,18 @@ var CeisumLayerRenderer = function (_maptalks$renderer$Ca) {
             destination: destination,
             orientation: orientation
         });
-        // const cameraPosition = map.pointToCoord(new maptalks.Coordinate(map.cameraPosition), map.getGLZoom());
-        // const resolution = map.getResolution();
-        // const distance = this._calcDistanceForResolution(
-        //     // resolution || 0, toRadians(ll[1]));
-        //     resolution, map);
-        var distance = this._calcDistance(map);
 
         scene.camera.moveBackward(distance);
     };
 
     CeisumLayerRenderer.prototype._calcDistance = function _calcDistance(map) {
         var canvas = this.canvas;
-        var fovy = this.scene.camera.frustum.fov; // horizontal field of view
+        var fov = this.scene.camera.frustum.fov; // horizontal field of view
 
         var c = map.getCenter();
-        var b = map.locateByPoint(c, canvas.width, 0);
-        var requiredDistance = map.computeLength(c, b) / 2 / Math.tan(fovy / 2);
-
-        // const pitch = toRadians(map.getPitch());
-        return requiredDistance;
-    };
-
-    CeisumLayerRenderer.prototype._calcDistanceForResolution2 = function _calcDistanceForResolution2(resolution, latitude) {
-        var canvas = this.canvas;
-        var fovy = this.scene.camera.frustum.fov; // vertical field of view
-        var metersPerUnit = resolution; //2 * Math.PI * 6370997 / 360;
-
-        // number of "map units" visible in 2D (vertically)
-        var visibleMapUnits = canvas.width;
-
-        // The metersPerUnit does not take latitude into account, but it should
-        // be lower with increasing latitude -- we have to compensate.
-        // In 3D it is not possible to maintain the resolution at more than one point,
-        // so it only makes sense to use the latitude of the "target" point.
-        var relativeCircumference = Math.cos(Math.abs(latitude));
-
-        // how many meters should be visible in 3D
-        var visibleMeters = visibleMapUnits * metersPerUnit * relativeCircumference;
-
-        // distance required to view the calculated length in meters
-        //
-        //  fovy/2
-        //    |\
-        //  x | \
-        //    |--\
-        // visibleMeters/2
-        var requiredDistance = visibleMeters / 2 / Math.tan(fovy / 2);
-
-        // NOTE: This calculation is not absolutely precise, because metersPerUnit
-        // is a great simplification. It does not take ellipsoid/terrain into account.
+        var b = map.locateByPoint(c, -canvas.width / 2, 0);
+        var e = map.locateByPoint(c, canvas.width / 2, 0);
+        var requiredDistance = map.computeLength(e, b) / 2 / Math.tan(fov / 2);
 
         return requiredDistance;
     };
